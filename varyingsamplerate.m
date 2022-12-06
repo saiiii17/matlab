74b0cd23f8139f781f0406dcf7f84305a084a54b
saiiii17 <87439489+saiiii17@users.noreply.github.com>
2022-12-06 19:38:12 +0530

    Create varyingsamplerate.m

---
+++ varyingsamplerate.m
@@ -0,0 +1,17 @@
+Fs = 44.1e3;
+t = 0:1/Fs:1-1/Fs;
+x = cos(2*pi*2000*t) + 1/2*sin(2*pi*4000*(t-pi/4)) + 1/4*cos(2*pi*8000*t);
+[P,Q] = rat(48e3/Fs);
+y=abs(P/Q*Fs-48000);
+disp(y);
+xnew = resample(x,P,Q);
+% sound(x,44100)
+% sound(xnew,48000)
+%load speech sample
+load mtlb
+[P,Q] = rat(8192/Fs);
+mtlb_new = resample(mtlb,P,Q);
+subplot(2,1,1)
+plot((0:length(mtlb)-1)/Fs,mtlb)
+subplot(2,1,2)
+plot((0:length(mtlb_new)-1)/(P/Q*Fs),mtlb_new)
